d731cd1c18b307580081791bd186cb0de54286a9
Winnie Shen <shinery_shenwei@sina.com>
2017-03-15 18:55:04 +0800

    Create index.js

---
+++ baidu-Season2/base-lesson/School-yaoyao/task4/index.js
@@ -0,0 +1,20 @@
+
+var ahead = 0; // 0-top,1-right,2-bottom,3-left
+
+function validateshift(top,left){
+    if(top<40){
+        alert(0);
+    }
+}
+
+function triggerAction(){
+    var action = document.getElementById("do-action").value;
+    var redObj = document.getElementById("red-obj");
+    var blueObj = document.getElementById("blue-obj");
+    if(value == "go"){
+        ahead = 0;
+        if(redObj)
+    }
+}
+
+document.getElementById("start-action").onclick = triggerAction;
